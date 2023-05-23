6bbd4deecbb091a24be7d5003b451dc6d3dba8cc
d.dankovskyi <d.dankovskyi@gmail.com>
2023-05-23 17:57:14 +0300

    Scene transition steps; Character movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json.Schema;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -13,14 +14,14 @@ public class Character : MonoBehaviour
     [SerializeField] private float _movespeed = 1f; // TODO take it form char manager
     PlayerInputActions.CharacterActions _inputActions;
     Camera _camera;
-    Rigidbody2D _rb;
+    CharacterController _characterController;
 
     void Start()
     {
         _camera = Camera.main;
         WandItem vandItem = Game.Get<InventoryManager<InventoryItem>>().Get(InventoryIds.WANDS_SLOTS_ID_PREFIX + 1) as WandItem;
         wand.Init(vandItem);
-        _rb = GetComponent<Rigidbody2D>();
+        _characterController = GetComponent<CharacterController>();
         InitInputs();
     }
 
@@ -42,13 +43,12 @@ public class Character : MonoBehaviour
     public void MovePlayer()
     {
         Vector2 input = _inputActions.Move.ReadValue<Vector2>();
-        _rb.MovePosition(_rb.position + input * _movespeed * Time.fixedDeltaTime);
-        //_rb.AddForce(input * _movespeed, ForceMode2D.Impulse);
-    }
+        Vector3 input3 = new Vector3(input.x, 0, input.y);
+        input3 = Quaternion.AngleAxis(_camera.transform.rotation.eulerAngles.y, Vector3.up) * input3;
+        if (input == Vector2.zero) return;
 
-    private void FixedUpdate()
-    {
-        MovePlayer();
+        _characterController.Move(input3 * _movespeed * Time.deltaTime);
+       
     }
 
 
@@ -56,16 +56,26 @@ public class Character : MonoBehaviour
     void Update()
     {
         RotateToCursor();
+        MovePlayer();
         HandleShooting();
     }
 
     void RotateToCursor()
     {
-        var mousePos = _camera.ScreenToWorldPoint(_inputActions.Aim.ReadValue<Vector2>());  // TODO Change to new Input system
-        mousePos.z = 0f;
-        var dir = (mousePos - wand.transform.position).normalized;
-        dir.z = 0;
-        wand.transform.up = dir;
+        //var mousePos = _camera.ScreenToWorldPoint(_inputActions.Aim.ReadValue<Vector2>());  // TODO Change to new Input system
+        //mousePos.z = 0f;
+
+        var ray = _camera.ScreenPointToRay(_inputActions.Aim.ReadValue<Vector2>());
+        if (Physics.Raycast(ray, out RaycastHit hit, float.MaxValue, LayerMask.GetMask("Ground"))) // TODO get rid of string id
+        {
+            var dir = (hit.point - transform.position).normalized;
+
+            transform.forward = dir;
+            //transform.LookAt(hit.point);
+        }
+        //var dir = (mousePos - wand.transform.position).normalized;
+        //dir.z = 0;
+        //wand.transform.up = dir;
     }
 
 
